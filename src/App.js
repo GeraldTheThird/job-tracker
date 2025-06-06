31d4529c55a83f52f0664d0623242ceb345caa73
Gerald Jones III <geraldjonesbusiness@gmail.com>
2025-06-06 14:55:06 -0400

    Initial commit: React app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Make sure this file exists
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="hero-banner">
+        <h1 className="hero-title">DevQuest</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
